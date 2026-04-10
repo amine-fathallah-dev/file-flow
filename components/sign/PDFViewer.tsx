b7dadb21ad443ad0581aff0761540f459ddb6f48
amine-fathallah-dev <amine.fathallah.dev@gmail.com>
2026-04-10 20:08:09 +0200

    03: rebuild and deploy

--- components/sign/PDFViewer.tsx
+++ components/sign/PDFViewer.tsx
@@ -7,10 +7,7 @@ import 'react-pdf/dist/Page/TextLayer.css';
 import Button from '@/components/ui/Button';
 
 // Use local worker bundled via webpack alias
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 interface PDFViewerProps {
   fileUrl: string;
